eb494bf1e92a1f2aadabc914574308c73990d2b4
roybarak80 <barakroy@gmail.com>
2018-10-16 23:47:40 +0300

    languages add

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-var app = angular.module('myApp', []);
+
+
+
+var app = angular.module('myApp', ['ngAnimate', 'ui.bootstrap']);
 
 app.controller('Ctrl', function ($scope) {
+
+
+    $scope.primaryLanguage ='english';
+    $scope.secondaryLanguage = 'german';
+    $scope.inputTranslated ='';
+    //----------------------------
+
 
     $scope.currLanguage = 'english';
     $scope.isWriteMode = false;
    
     
     $scope.storedOutput = [];
+    $scope.storedInputTranslated = [];
     $scope.lastTraslatedChar = '';
 
     $scope.changeLanguage = function(lanStr){
 
         
-       var german = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        var german = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         var chinese = ".!@#$5G9P06*<>?:{}[_+]AcErP~,`";
 
          //Switch language 
@@ -38,6 +49,47 @@ app.controller('Ctrl', function ($scope) {
         $scope.storedOutput = [];
         $scope.input = [];
         $scope.isWriteMode = false;
+
+    }
+
+    $scope.translateHandle = function(){
+        
+        $scope.isWriteMode = true;
+      //  var currInputText = $scope.inputTranslated;
+        var german = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        var chinese = ".!@#$5G9P06*<>?:{}[_+]AcErP~,`";
+
+         //Switch language 
+        switch($scope.primaryLanguage){
+            case 'german':
+            var currLanguage = german;
+            break;
+
+            case 'chinese':
+            var currLanguage = chinese;
+            break;
+
+            case 'english':
+            var currLanguage = [];
+            break;
+
+          
+
+        }
+
+        if(currLanguage.length >0){
+            var currInputTextArr = $scope.inputTranslated.split('');
+
+            var lastTypedLetter = currInputTextArr[currInputTextArr.length - 1];
+            var rnum = Math.floor(Math.random() * currLanguage.length);
+            lastTypedLetter = currLanguage.substring(rnum, rnum + 1);
+            $scope.storedInputTranslated.push(lastTypedLetter);
+            $scope.traslatedTextStr = $scope.storedInputTranslated.join('');
+    
+            //  console.log(storedLastTranslatedChar);
+            $scope.inputTranslated = $scope.traslatedTextStr;
+        }
+      
 
     }
     $scope.translateStr = function (e) {
@@ -84,7 +136,7 @@ app.controller('Ctrl', function ($scope) {
                 } else {
                     var rnum = Math.floor(Math.random() * currSelectesdLanguage.length);
                     lastTypedLetter = currSelectesdLanguage.substring(rnum, rnum + 1);
-                    $scope.lastTraslatedChar = lastTypedLetter;
+                   // $scope.lastTraslatedChar = lastTypedLetter;
 
                     $scope.storedOutput.push(lastTypedLetter);
                 }
